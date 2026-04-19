707cfa8576e27ca404222c852b7bee4bea7f179c
piranavan04 <piranavan0604@gmail.com>
2026-04-19 13:05:06 +0530

    Edit field names of student table in database

--- crud/src/main/java/com/sgic/crud/entity/Student.java
+++ crud/src/main/java/com/sgic/crud/entity/Student.java
@@ -1,20 +1,19 @@
 package com.sgic.crud.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "student")
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(name = "f_name")
     private String first_name;
-    private String last_name;
 
+    @Column(name = "l_name")
+    private String last_name;
 }
